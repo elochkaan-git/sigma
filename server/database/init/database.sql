0812fb89125209b6c6ee6cce0dc7ad8552475775
Alexander Elka <elochkaan@gmail.com>
2026-07-01 14:01:57 +0700

    Docker compose file for creating database and startup script

---
+++ server/database/init/database.sql
@@ -0,0 +1,31 @@
+-- Script for creating database with tables
+
+create table if not exists users (
+  id serial primary key,
+  login varchar(30) unique not null,
+  passwd text not null,
+  last_seen timestamp default null
+);
+
+create table if not exists msgs_query (
+  id serial primary key,
+  sender_id int not null,
+  receiver_id int not null,
+  encrypted_content bytea not null,
+  sent timestamp not null,
+  delivered varchar(10) not null default 'sent',
+  constraint fk_sender foreign key (sender_id) references users(id),
+  constraint fk_receiver foreign key (receiver_id) references users(id),
+  constraint chk_sender_receiver check (sender_id != receiver_id)
+);
+
+create table if not exists relations (
+  id serial primary key,
+  user_id int not null,
+  friend_id int not null,
+  status varchar(10) not null,
+  constraint fk_user foreign key (user_id) references users(id),
+  constraint fk_friend foreign key (friend_id) references users(id),
+  constraint uc_user_friend unique (user_id, friend_id),
+  constraint chk_user_friend check (user_id != friend_id)
+);
