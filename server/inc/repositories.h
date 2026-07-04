3ad386ac4eed7c7020b72061ad015aa1e9425b01
Alexander Elka <elochkaan@gmail.com>
2026-07-04 10:18:31 +0700

    Add docs for UserRepository

--- server/inc/repositories.h
+++ server/inc/repositories.h
@@ -3,10 +3,29 @@
 #include "statuses.h"
 #include <QString>
 
+/**
+ * @brief Класс для непосредственной работы с таблицами в базе данных
+ * 
+ */
 class UserRepository
 {
 public:
+  /**
+   * @brief Конструктор UserRepository
+   * 
+   * @param manager указатель на ConnectionManager
+   * @see ConnectionManager
+   */
   UserRepository(ConnectionManager* manager);
+  /**
+   * @brief Метод регистрации пользователя. Принимает логин и хэшированный пароль.
+   * В случае успеха, возвращает ОК, при существовании пользователя возвращает UserExist.
+   * 
+   * @param login логин пользователя
+   * @param pwd_hash хэш пароля
+   * @return OperationStatus статус выполнения операции
+   * @see OperationStatus
+   */
   OperationStatus registerUser(QString login, QString pwd_hash);
 
 private:
