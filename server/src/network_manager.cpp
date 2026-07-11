34277a4b5db52c31c7ca87edfb10ebe971bc9537
Alexander Elka <elochkaan@gmail.com>
2026-07-11 14:57:56 +0700

    Basic anti-flood system Ban by ip or by id for brute forcing or sending many too big messages

--- server/src/network_manager.cpp
+++ server/src/network_manager.cpp
@@ -16,9 +16,16 @@
 #include <QWebSocket>
 #include <QWebSocketServer>
 #include <QtTypes>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
 #include <optional>
+#include <qdatetime.h>
+#include <qhostaddress.h>
+#include <qiterable.h>
 #include <qjsonvalue.h>
 #include <qlogging.h>
+#include <qtypes.h>
 #include <qvariant.h>
 #include <variant>
 
@@ -198,13 +205,124 @@ NetworkManager::onNewConnection()
   qInfo(appNetwork) << "New connection! ID: " << newConnectionId;
 }
 
+// Перевести на чтение бинарных данных, а не текстовых
 void
 NetworkManager::onMessageReceived(const QString& message)
 {
-  const QByteArray data(message.toStdString());
-  QUuid client_id =
-    qobject_cast<QWebSocket*>(this->sender())->property("client_id").toUuid();
-  Command cmd = this->deserialize(client_id, data);
+  qDebug(appNetwork) << "Size of message:" << message.size();
+  qsizetype maxMsgSize = 536'870'912;
+  QWebSocket* client = qobject_cast<QWebSocket*>(this->sender());
+  QHostAddress addr(client->request().headers().value("IPv4").toByteArray());
+  QUuid client_id = client->property("client_id").toUuid();
+
+  QDateTime now = QDateTime::currentDateTime();
+  Command cmd;
+  QVariant uid = client->property("user_id");
+  std::variant<decltype(mIDConstraints)::iterator,
+               decltype(mIPConstraints)::iterator>
+    user;
+
+  if (uid.isValid()) {
+    user = mIDConstraints.find(uid.toUInt());
+    auto IDuser = std::get<decltype(mIDConstraints)::iterator>(user);
+    if (IDuser == mIDConstraints.end()) {
+      mIDConstraints[uid.toUInt()] =
+        ConnectionState{ ConnectionStatus::COMMON, {}, now };
+    }
+    user = mIDConstraints.find(uid.toUInt());
+  } else {
+    user = mIPConstraints.find(addr);
+    auto IPuser = std::get<decltype(mIPConstraints)::iterator>(user);
+    if (IPuser == mIPConstraints.end()) {
+      // TODO: с появлением кофигурации сделать обращение к peerAddress, если
+      // передача через заголовок отключена
+      mIPConstraints[addr] =
+        ConnectionState{ ConnectionStatus::COMMON, {}, now };
+    }
+    user = mIPConstraints.find(addr);
+  }
+
+  ConnectionState& state =
+    std::visit([](auto&& it) -> auto& { return *it; }, user);
+  CommandType type;
+
+  if (message.size() > maxMsgSize) {
+    // TODO: сделать небольшую функцию, которая бы в зависимости от размера
+    // писала mb, kb и т.д.
+    QString reason("Message size bigger than %1 MB");
+    cmd = Error{ client_id, reason.arg(maxMsgSize / 1024 / 1024) };
+    type = CommandType::OVERSIZED;
+  } else {
+    const QByteArray data(message.toStdString());
+    cmd = this->deserialize(client_id, data);
+    type = this->getTypeOfCommand(cmd);
+  }
+
+  using namespace std::chrono;
+  if (state.status != ConnectionStatus::COMMON) {
+    long timeLimitMinutes = 0;
+    QString reason = "";
+    milliseconds delta = now - state.timestamp;
+    switch (state.status) {
+      case ConnectionStatus::FLOOD:
+        timeLimitMinutes = 15;
+        reason = "FLOOD";
+        break;
+      case ConnectionStatus::BAN:
+        timeLimitMinutes = 60;
+        reason = "BAN";
+        break;
+      default:
+        timeLimitMinutes = 15;
+        reason = "UNKNOWN";
+        break;
+    }
+    if (duration_cast<minutes>(delta).count() < timeLimitMinutes) {
+      cmd =
+        Error{ client_id,
+               QString("You cannot send messages due to a %1").arg(reason) };
+    } else {
+      state.status = ConnectionStatus::COMMON;
+      state.last_cmds.clear();
+      state.last_cmds.append({ type, now });
+    }
+  } else {
+    state.last_cmds.removeIf([&](const Record& r) {
+      return duration_cast<minutes>(now - r.timestamp).count() > 15;
+    });
+    // TODO: подсчет всех типов, вместо if для каждого типа
+    if (std::count_if(
+          state.last_cmds.begin(), state.last_cmds.end(), [](const Record& r) {
+            return r.type == CommandType::LOGIN;
+          }) > 4) {
+      cmd =
+        Error{ client_id, QString("Too many login attempts. Now you in ban") };
+      state.last_cmds.clear();
+      state.status = ConnectionStatus::BAN;
+      state.timestamp = now;
+    } else if (std::count_if(state.last_cmds.begin(),
+                             state.last_cmds.end(),
+                             [](const Record& r) {
+                               return r.type == CommandType::OVERSIZED;
+                             }) > 4) {
+      cmd = Error{ client_id,
+                   QString("Too many big messages. Now you in ban") };
+      state.last_cmds.clear();
+      state.status = ConnectionStatus::BAN;
+      state.timestamp = now;
+    } else if (state.last_cmds.size() > 50) {
+      state.status = ConnectionStatus::FLOOD;
+      state.timestamp = now;
+      cmd = Error{
+        client_id,
+        QString(
+          "Too many messages. Now you block for 15 minutes due to flood")
+      };
+    } else {
+      state.last_cmds.append({ type, now });
+    }
+  }
+
   mDispatcher->dispatch(cmd, this, [this](const Response& r) {
     this->handleSideEffect(r);
     this->sendResponse(r);
@@ -233,6 +351,26 @@ NetworkManager::getClientId(const Response& response)
 {
   return std::visit(overloaded{ [](const auto& r) { return r.client_id; } },
                     response);
+}
+
+CommandType
+NetworkManager::getTypeOfCommand(const Command& cmd)
+{
+  return std::visit(
+    overloaded{
+      [](const Error&) { return CommandType::ERROR; },
+      [](const RegisterUser&) { return CommandType::REGISTER; },
+      [](const LoginUser&) { return CommandType::LOGIN; },
+      [](const SendMessage&) { return CommandType::SEND_MESSAGE; },
+      [](const SendFriendRequest&) { return CommandType::SEND_FRIEND_REQUEST; },
+      [](const AcceptFriendRequest&) {
+        return CommandType::ACCEPT_FRIEND_REQUEST;
+      },
+      [](const RejectFriendRequest&) {
+        return CommandType::REJECT_FRIEND_REQUEST;
+      },
+      [](const RemoveFriend&) { return CommandType::REMOVE_FRIEND; } },
+    cmd);
 }
 
 void
