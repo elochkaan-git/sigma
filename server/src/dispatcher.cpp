2589f7ead379c5c38e4a1d7c833b2cc29fe4e9e9
Alexander Elka <elochkaan@gmail.com>
2026-07-15 11:36:15 +0700

    Add logging

--- server/src/dispatcher.cpp
+++ server/src/dispatcher.cpp
@@ -2,6 +2,7 @@
 
 #include "command_types.h"
 #include "commands.h"
+#include "logging.h"
 #include "responses.h"
 #include "services.h"
 #include "structures.h"
@@ -13,6 +14,7 @@
 #include <functional>
 #include <memory>
 #include <optional>
+#include <qlogging.h>
 #include <vector>
 
 Dispatcher::Dispatcher(Services services, OnlineUsersRegistry* registry)
@@ -27,6 +29,8 @@ Dispatcher::dispatch(const Command& cmd,
                      QObject* context,
                      std::function<void(Response)> onResponseReady)
 {
+  qInfo(appDispatcher) << "Got new command: " +
+                            commandTypeToString(getTypeOfCommand(cmd));
   std::function<std::vector<Response>()> job = std::visit(
     overloaded{
       [this](
@@ -103,6 +107,13 @@ Dispatcher::handleRegisterUser(const RegisterUser& cmd)
 {
   OperationStatus status =
     this->mServices.u_service->registerUser(cmd.login, cmd.pwd);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command RegisterUser return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("User %1 registered").arg(cmd.client_id.toString());
+  }
   return std::vector<Response>{ RegisterUserResponse{ cmd.client_id, status } };
 }
 
@@ -119,11 +130,23 @@ Dispatcher::handleLoginUser(const LoginUser& cmd)
       for (const auto& m : msgs.value()) {
         responses.push_back(
           NewMessageResponse{ cmd.client_id, m.sender_id, m.content });
-        this->mServices.msg_service->deleteFromQueue(m.msg_id);
+        status = this->mServices.msg_service->deleteFromQueue(m.msg_id);
+        if (status != OperationStatus::OK) {
+          qWarning(appDispatcher)
+            << QString("Command LoginUser return status %1").arg((int)status);
+          break;
+        }
+      }
+      if (status != OperationStatus::OK) {
+        qWarning(appDispatcher) << "Not all messages from queue was processed";
+      } else {
+        qInfo(appDispatcher) << "All messages from queue was processed";
       }
     }
     return responses;
   } else {
+    qWarning(appDispatcher)
+      << QString("Command LoginUser return status %1").arg((int)status);
     return std::vector<Response>{ LoginUserResponse{
       cmd.client_id, 0, OperationStatus::InvalidCredentials } };
   }
@@ -135,17 +158,24 @@ Dispatcher::handleMessage(const SendMessage& cmd)
   OperationStatus status =
     this->mServices.rel_service->areFriends(cmd.sender_id, cmd.receiver_id);
   if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command SendMessage return status %1").arg((int)status);
     return std::vector<Response>{ SendMessageResponse{ cmd.client_id,
                                                        status } };
   }
   std::optional<QUuid> receiver_id =
     this->mRegistry->getClientId(cmd.receiver_id);
   if (receiver_id.has_value()) {
+    qInfo(appDispatcher) << QString("Sending message from %1 to %2")
+                              .arg(cmd.sender_id)
+                              .arg(cmd.receiver_id);
     return std::vector<Response>{
       SendMessageResponse{ cmd.client_id, OperationStatus::OK },
       NewMessageResponse{ receiver_id.value(), cmd.sender_id, cmd.content }
     };
   } else {
+    qInfo(appDispatcher)
+      << QString("Saving message from %1 in queue").arg(cmd.sender_id);
     OperationStatus status = mServices.msg_service->saveToQueue(
       cmd.sender_id, cmd.receiver_id, cmd.content);
     return std::vector<Response>{ SendMessageResponse{ cmd.client_id,
@@ -156,8 +186,17 @@ Dispatcher::handleMessage(const SendMessage& cmd)
 std::vector<Response>
 Dispatcher::handleSendFriendRequest(const SendFriendRequest& cmd)
 {
+
   OperationStatus status =
     this->mServices.rel_service->sendFriendRequest(cmd.user_id, cmd.friend_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command SendFriendRequest return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher) << QString("Sending friend request from %1 to %2")
+                              .arg(cmd.user_id)
+                              .arg(cmd.friend_id);
+  }
   return std::vector<Response>{ SendFriendRequestResponse{ cmd.client_id,
                                                            status } };
 }
@@ -167,6 +206,14 @@ Dispatcher::handleAcceptFriendRequest(const AcceptFriendRequest& cmd)
 {
   OperationStatus status = this->mServices.rel_service->acceptFriendRequest(
     cmd.user_id, cmd.friend_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command AcceptFriendRequest return status %1")
+           .arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("%1 and %2 friends now").arg(cmd.user_id).arg(cmd.friend_id);
+  }
   return std::vector<Response>{ AcceptFriendRequestResponse{ cmd.client_id,
                                                              status } };
 }
@@ -176,6 +223,14 @@ Dispatcher::handleRejectFriendRequest(const RejectFriendRequest& cmd)
 {
   OperationStatus status = this->mServices.rel_service->rejectFriendRequest(
     cmd.user_id, cmd.friend_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command RejectFriendRequest return status %1")
+           .arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("%1 reject %2").arg(cmd.user_id).arg(cmd.friend_id);
+  }
   return std::vector<Response>{ RejectFriendRequestResponse{ cmd.client_id,
                                                              status } };
 }
@@ -185,14 +240,28 @@ Dispatcher::handleRemoveFriend(const RemoveFriend& cmd)
 {
   OperationStatus status =
     this->mServices.rel_service->removeFriend(cmd.user_id, cmd.friend_id);
-  return std::vector<Response>{ SendFriendRequestResponse{ cmd.client_id,
-                                                           status } };
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command RemoveFriend return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher) << QString("%1 not %2 friends anymore")
+                              .arg(cmd.user_id)
+                              .arg(cmd.friend_id);
+  }
+  return std::vector<Response>{ RemoveFriendResponse{ cmd.client_id, status } };
 }
 
 std::vector<Response>
 Dispatcher::handleGetFriends(const GetFriends& cmd)
 {
   auto [status, friends] = this->mServices.rel_service->getFriends(cmd.user_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command GetFriends return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("Sending friends list to %1").arg(cmd.user_id);
+  }
   return std::vector<Response>{ GetFriendsResponse{
     cmd.client_id, status, friends } };
 }
@@ -202,6 +271,13 @@ Dispatcher::handleGetFriendRequests(const GetFriendRequests& cmd)
 {
   auto [status, requests] =
     this->mServices.rel_service->getFriendRequests(cmd.user_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command GetFriendRequests return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("Sending friend requests list to %1").arg(cmd.user_id);
+  }
   return std::vector<Response>{ GetFriendRequestsResponse{
     cmd.client_id, status, requests } };
 }
@@ -211,6 +287,13 @@ Dispatcher::handleGetSentFriendRequests(const GetSentFriendRequests& cmd)
 {
   auto [status, sentRequests] =
     this->mServices.rel_service->getSentFriendRequests(cmd.user_id);
+  if (status != OperationStatus::OK) {
+    qWarning(appDispatcher)
+      << QString("Command GetFriendRequests return status %1").arg((int)status);
+  } else {
+    qInfo(appDispatcher)
+      << QString("Sending sent friend requests list to %1").arg(cmd.user_id);
+  }
   return std::vector<Response>{ GetSentFriendRequestsResponse{
     cmd.client_id, status, sentRequests } };
 }
--- server/src/services.cpp
+++ server/src/services.cpp
@@ -1,9 +1,11 @@
 #include "services.h"
 
+#include "logging.h"
 #include "repositories.h"
 #include "structures.h"
 
 #include <QString>
+#include <qlogging.h>
 #include <sodium.h>
 
 #include <optional>
@@ -22,12 +24,16 @@ UserService::registerUser(QString login, QString passwd)
 {
   char pwd_hash_c[crypto_pwhash_STRBYTES];
   std::string pwd_string = passwd.toStdString();
+  qInfo(appService) << QString("Computing password hash of %1").arg(login);
   if (crypto_pwhash_str(pwd_hash_c,
                         pwd_string.c_str(),
                         pwd_string.length(),
                         crypto_pwhash_OPSLIMIT_INTERACTIVE,
                         crypto_pwhash_MEMLIMIT_INTERACTIVE) != 0) {
+    qFatal(appService) << "Out of memory while computing hash";
     throw std::runtime_error("Out of memory");
+  } else {
+    qInfo(appService) << QString("Password hash of %1 computed").arg(login);
   }
   QString pwd_hash(pwd_hash_c);
   return mUserRepo->registerUser(login, pwd_hash);
@@ -38,6 +44,7 @@ UserService::loginUser(QString login, QString passwd)
 {
   auto [status, user_info] = mUserRepo->getUserByLogin(login);
   if (status == OperationStatus::UserNotExist) {
+    qWarning(appService) << QString("User %1 not exists").arg(login);
     return { OperationStatus::UserNotExist, std::nullopt };
   } else {
     std::string pwd = passwd.toStdString();
@@ -45,6 +52,7 @@ UserService::loginUser(QString login, QString passwd)
           user_info.value().pwd_hash.toStdString().c_str(),
           pwd.c_str(),
           pwd.length()) != 0) {
+      qWarning(appService) << QString("Wrong password, %1").arg(login);
       return { OperationStatus::InvalidCredentials, std::nullopt };
     }
     return { OperationStatus::OK, user_info.value().user_id };
@@ -120,10 +128,15 @@ RelationService::getFriends(unsigned int user_id)
 {
   const auto [status_users, friends] = this->mRelRepo->getFriendsID(user_id);
   if (status_users != OperationStatus::OK || !friends.has_value()) {
+    qWarning(appService) << QString("User %1 have no friends or have error %2")
+                              .arg(user_id)
+                              .arg((int)status_users);
     return { status_users, std::nullopt };
   }
   auto [status_rel, users] = this->mUserRepo->getUsersById(friends.value());
   if (status_rel != OperationStatus::OK || !users.has_value()) {
+    qWarning(appService)
+      << QString("No users with such ids or error %1").arg((int)status_users);
     return { status_rel, std::nullopt };
   }
   for (auto& u : users.value()) {
@@ -138,10 +151,16 @@ RelationService::getFriendRequests(unsigned int user_id)
   const auto [status_users, requests] =
     this->mRelRepo->getFriendRequests(user_id);
   if (status_users != OperationStatus::OK || !requests.has_value()) {
+    qWarning(appService)
+      << QString("User %1 have no friend requests or have error %2")
+           .arg(user_id)
+           .arg((int)status_users);
     return { status_users, std::nullopt };
   }
   auto [status_rel, users] = this->mUserRepo->getUsersById(requests.value());
   if (status_rel != OperationStatus::OK || !users.has_value()) {
+    qWarning(appService)
+      << QString("No users with such ids or error %1").arg((int)status_users);
     return { status_rel, std::nullopt };
   }
   for (auto& u : users.value()) {
@@ -156,11 +175,17 @@ RelationService::getSentFriendRequests(unsigned int user_id)
   const auto [status_users, sentRequests] =
     this->mRelRepo->getSentFriendRequests(user_id);
   if (status_users != OperationStatus::OK || !sentRequests.has_value()) {
+    qWarning(appService)
+      << QString("User %1 have no sent friend requests or have error %2")
+           .arg(user_id)
+           .arg((int)status_users);
     return { status_users, std::nullopt };
   }
   auto [status_rel, users] =
     this->mUserRepo->getUsersById(sentRequests.value());
   if (status_rel != OperationStatus::OK || !users.has_value()) {
+    qWarning(appService)
+      << QString("No users with such ids or error %1").arg((int)status_users);
     return { status_rel, std::nullopt };
   }
   for (auto& u : users.value()) {
