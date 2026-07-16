68f9ac76b6be9036ae0329fab5c7a7297c587f94
Savely Burkovskiy <savelybur.work@gmail.com>
2026-07-17 05:34:23 +0700

    Added special client controller logic that will hadle data and interface on client

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include "client_controller.hpp" // Подключаем наш контроллер
 
 int main(int argc, char *argv[])
 {
-  qputenv("QT_MESSAGE_PATTERN", "[%{time h:mm:ss.zzz}] %{type}: %{message}");
-  QGuiApplication app(argc, argv);
+    qputenv("QT_MESSAGE_PATTERN", "[%{time h:mm:ss.zzz}] %{type}: %{message}");
+    QGuiApplication app(argc, argv);
 
-  QQmlApplicationEngine engine;
-  QObject::connect(
-      &engine,
-      &QQmlApplicationEngine::objectCreationFailed,
-      &app,
-      []() { QCoreApplication::exit(-1); },
-      Qt::QueuedConnection);
+    // 1. Создаем экземпляр контроллера
+    ClientController clientController;
 
-  engine.load("qrc:/Main/qml/Main.qml");
-  return app.exec();
+    QQmlApplicationEngine engine;
+
+    // 2. Внедряем его в корневой контекст QML под именем "clientController"
+    engine.rootContext()->setContextProperty("clientController", &clientController);
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    // Обратите внимание: стартовать лучше с Main.qml, где у вас StackView с выбором сервера!
+    engine.load("qrc:/Main/qml/Main.qml"); 
+    return app.exec();
 }
