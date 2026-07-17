7e0b0990c8611e9ade96e109e42d73271dba9573
Alexander Elka <elochkaan@gmail.com>
2026-07-17 14:29:30 +0700

    Registry for calls

---
+++ server/src/call_registry.cpp
@@ -0,0 +1,42 @@
+#include "call_registry.h"
+
+#include <QUuid>
+#include <QReadLocker>
+#include <QWriteLocker>
+
+#include <optional>
+
+QUuid
+CallRegistry::createRecord(unsigned int caller_id, unsigned int callee_id)
+{
+  QWriteLocker locker(&mLock);
+  QUuid call_id = QUuid::createUuid();
+  mCalls[call_id] = {caller_id, callee_id, CallStatus::Ringing};
+  return call_id;
+}
+
+void
+CallRegistry::deleteRecord(const QUuid& call_id)
+{
+  QWriteLocker locker(&mLock);
+  mCalls.remove(call_id);
+}
+
+void
+CallRegistry::updateRecord(const QUuid& call_id, CallStatus new_status)
+{
+  QWriteLocker locker(&mLock);
+  if (mCalls.contains(call_id)) {
+    mCalls[call_id].status = new_status;
+  }
+}
+
+std::optional<CallRecord>
+CallRegistry::getCallRecord(const QUuid& call_id)
+{
+  QReadLocker locker(&mLock);
+  if (!mCalls.contains(call_id)) {
+    return std::nullopt;
+  }
+  return mCalls.value(call_id);
+}
