3148ae7c063cb5c5fd3f5530b61657f1959b0dda
Alexander Elka <elochkaan@gmail.com>
2026-07-19 00:06:32 +0700

    Logging in call registry

--- server/src/call_registry.cpp
+++ server/src/call_registry.cpp
@@ -1,5 +1,7 @@
 #include "call_registry.h"
 
+#include "logging.h"
+
 #include <QReadLocker>
 #include <QUuid>
 #include <QWriteLocker>
@@ -11,6 +13,10 @@ CallRegistry::createRecord(unsigned int caller_id, unsigned int callee_id)
 {
   QWriteLocker locker(&mLock);
   QUuid call_id = QUuid::createUuid();
+  qDebug(appCalls) << QString("Creating call %1 for %2 and %3")
+                                .arg(call_id.toString())
+                                .arg(caller_id)
+                                .arg(callee_id);
   mCalls[call_id] = { caller_id, callee_id, CallStatus::Ringing };
   return call_id;
 }
@@ -19,6 +25,7 @@ void
 CallRegistry::deleteRecord(const QUuid& call_id)
 {
   QWriteLocker locker(&mLock);
+  qDebug(appCalls) << QString("Deleting call %1").arg(call_id.toString());
   mCalls.remove(call_id);
 }
 
@@ -28,6 +35,9 @@ CallRegistry::updateRecord(const QUuid& call_id, CallStatus new_status)
   QWriteLocker locker(&mLock);
   if (mCalls.contains(call_id)) {
     mCalls[call_id].status = new_status;
+    qDebug(appCalls) << QString("Setting status %1 to call %2")
+                                  .arg((int)new_status)
+                                  .arg(call_id.toString());
   }
 }
 
